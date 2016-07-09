e8e6115b89a77dff26e0cc5102d6946a4b6c2dfe
justincoh <justincoh@gmail.com>
2016-07-09 15:33:17 -0400

    Delete settings.py

--- download/settings.py
+++
@@ -1,6 +0,0 @@
-# JUSTIN NEVER RUN THIS ON YOUR WORK COMP
-REGION =  'us-west-2'
-ACCESS_KEY_ID = 'AKIAJTR44OGNTNOY3RJA'
-SECRET_ACCESS_KEY = 'vLW2mSdYaaItedx1ZlIjsSDTGXMWtXn2B+IjbgUt'
-BUCKET = 'background-backups'
-PATH = '/Users/justincohen/Documents/'
